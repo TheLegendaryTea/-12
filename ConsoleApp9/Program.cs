362c963fd007e239bc5e572a3a85fef429a5c4f3
su <su@DESKTOP-93TC6F5>
2021-10-05 13:14:23 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                try
+            {
+                Console.WriteLine("введите целые положительные числа (a>b)");
+                Console.WriteLine("введите а:");
+                int a = int.Parse(Console.ReadLine());
+                Console.WriteLine("введите b:");
+                int b = int.Parse(Console.ReadLine());
+                int B = a / b;
+                Console.WriteLine("B:{0}",B);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+
+            
+        }
+    
+
+
+        }
+    }
+}
